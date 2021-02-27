de1a8fa5ff601a43c5e5aeb02a28801d1cfb6261
Margaret Hamilton <hamiltonmargaret400@gmail.com>
2021-02-27 16:23:10 -0600

    Initial Commit

---
+++ HellloWorld/HellloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HellloWorldApp.swift
+//  HellloWorld
+//
+//  Created by Test on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct HellloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
